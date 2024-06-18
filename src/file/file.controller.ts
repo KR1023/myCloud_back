2a67fe09973d05a25348f88d5f76a8c5566e39f0
ysh <shyoo1023@gmail.com>
2024-06-18 17:06:14 +0900

    res file attr

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Post, Body, Param, Response } from '@nestjs/common';
-import { join, extname } from 'path';
+import { Controller, Get, Post, Body, Param, Response, HttpException, HttpStatus } from '@nestjs/common';
+import { join, basename, extname } from 'path';
 import * as fs from 'fs';
 import * as fs_promise from 'fs/promises';
 
@@ -27,6 +27,47 @@ export class FileController {
             return res.status(200).send(resList);
         }catch(e){
             console.error(e);
+        }
+    }
+
+    @Post('/create-dir')
+    async createDir(@Body() req, @Response() res){
+        const { userEmail, currDir, dirName } = req;
+
+        try{
+            const defaultPath = join(__dirname, '../../uploads/explorer', userEmail, currDir);
+            if(!fs.existsSync(join(defaultPath, dirName))){
+
+                fs.mkdirSync(join(defaultPath, dirName))
+                return res.status(200).send({"message" : "dir is created"});
+            }else{
+                throw new HttpException('Already Exists', HttpStatus.CONFLICT);
+            }
+            
+        }catch(e){
+            console.error(e);
+            return res.status(e.status).send(e.message);
+        }
+    }
+
+    @Post('/file-attr')
+    async getFileAttr(@Body() req, @Response() res){
+        const { filePath } = req;
+        
+        try{
+            const filename = basename(filePath);
+            // const fileAttr = fs.readdirSync(filePath, { withFileTypes: true});
+            const fileAttr = fs.statSync(filePath);
+            const { size, mtime, ctime, birthtime } = fileAttr;
+            const resAttr = {"filename": filename, size, birthtime, mtime, ctime};
+            return res.status(200).send(resAttr);
+        }catch(e){
+            console.error(e);
+            if(e.code === 'ENOENT'){
+                e.message = "file or directory is not found."
+                return res.status(404).send(e.message);
+            }
+            return res.status(500).send(e.message);
         }
     }
 }
